75a3c27f46a6020a9a794f64cfb1a4335e53abc4
kboul <kos.voulgaridis@gmail.com>
2019-10-03 11:25:28 +0300

    Enable parsing of JSON objects

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 
+// enable parsing of JSON objects
+// in the body of the request by
+// adding a piece of middleware...
+app.use(express.json());
+
 app.use('/api/genres', genres);
 
 const port = process.env.PORT || 3900;
