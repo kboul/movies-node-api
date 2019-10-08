142c268a121fdcac7cdff6046e2a071abafb9107
kboul <kos.voulgaridis@gmail.com>
2019-10-08 14:04:58 +0300

    Create validateAuth for the object that client will send to login

---
+++ validators/validateAuth.js
@@ -0,0 +1,23 @@
+const Joi = require('@hapi/joi');
+
+/**
+ *
+ * @param {Object} user
+ * @returns {Object} - validation error object
+ */
+
+const validateAuth = user => {
+    const schema = Joi.object({
+        email: Joi.string()
+            .min(5)
+            .max(255)
+            .required()
+            .email(),
+        password: Joi.string()
+            .min(5)
+            .max(255)
+    });
+    return schema.validate(user);
+};
+
+module.exports = validateAuth;
