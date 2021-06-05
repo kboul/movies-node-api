0a9328804a99c67f8f2e246af9cabc89992c8a66
kboul <kos.voulgaridis@gmail.com>
2021-06-05 13:53:37 +0300

    Install & use cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const config = require('config');
+const cors = require('cors');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
@@ -16,18 +17,26 @@ if (!config.get('jwtPrivateKey')) {
 
 const options = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 };
 
 mongoose
     .connect('mongodb://localhost/movies-db', options)
     .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+    .catch((err) => console.error('Could not connect to MongoDB...', err));
 
 mongoose.set('useCreateIndex', true);
 
 // enable parsing of JSON objects
 app.use(express.json());
+
+// CORS for react app, assuming port 3000
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    })
+);
 
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
